f27ce9490ca3479f0577adf5e5e0199b7d98b51f
Chinmay Pendharkar <notthetup@gmail.com>
2013-12-17 21:40:23 +0800

    added pipe support

--- newevents.js
+++ newevents.js
@@ -1,44 +1,26 @@
 #!/usr/bin/env node
 
-
-/*Requires
-Commander - input parsing etc.
-Request - HTTP request.
-
-Can be installed using npm
-
-npm install commander
-npm install request
-
-*/
-var program = require('commander');
 var request = require('request');
 
 /* Commander  Stuff*/
-program
-.version('0.0.1')
-.option('-i, --input <path>', 'Input GCal JSON file')
-.parse(process.argv);
+var stdin = process.openStdin();
+var data = "";
 
-if (!program.input){
-	console.log('error: option \'-i, --input <path>\' is mandatory ');
-	program.outputHelp();
-	return;
-}
+stdin.on('data', function(chunk) {
+  data += chunk;
+});
 
-// Get JSON form GCal
-request(program.input, function (error, response, body) {
-	if (!error && response.statusCode == 200) {
-		var hsgEvents = JSON.parse(body).feed.entry;
-		var futureEvents = [];
-		hsgEvents.map(function(cEvent){
-			if (cEvent.gd$when){
-				var timeStr = cEvent.gd$when[0].startTime;
-				if (Date.parse(timeStr) > Date.now()){
-					futureEvents.push({"Name" : cEvent.title.$t, "When" :  timeStr});
-				}
+stdin.on('end', function() {
+	console.log("got all data");
+	var hsgEvents = JSON.parse(data).feed.entry;
+	var futureEvents = [];
+	hsgEvents.map(function(cEvent){
+		if (cEvent.gd$when){
+			var timeStr = cEvent.gd$when[0].startTime;
+			if (Date.parse(timeStr) > Date.now()){
+				futureEvents.push({"Name" : cEvent.title.$t, "When" :  timeStr});
 			}
-		});
-	}
+		}
+	});
 	console.log(futureEvents);
 });
